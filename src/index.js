b74a4ffbecb92c168a040d89a8c3fce202d98360
Stu <stuart.judd.bsc@gmail.com>
2022-07-18 10:30:09 +0100

    redux-toolkit integration

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './style.css';
+import { store } from './redux-toolkit/store'
+import { Provider } from 'react-redux'
+
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
 root.render(
+	<Provider store={store}>
 		<App />
+	</Provider>	
 );
