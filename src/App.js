afac27d6ba1fe908f4f89e583983965c78a3ac49
Stu <stuart.judd.bsc@gmail.com>
2022-07-18 17:36:14 +0100

    landing page updates

--- src/App.js
+++ src/App.js
@@ -7,19 +7,17 @@ import pages from './pages';
 import component from './components';
 
 import logo from './assets/images/logo_large.png';
-import background from './assets/images/background8.jpg'
+import background from './assets/images/background8.jpg';
 import './style.css';
-
 
 const App = () => {
 	return (
 		<>
-			<div className="background-container" style={{ backgroundImage: `url(${background})`, backgroundRepeat:"no-repeat",backgroundSize:"cover", 
-    height:'100vh',width:'100vw'  }} >
+			<div className="background-container" style={{ backgroundImage: `url(${background})`, backgroundRepeat:"no-repeat",backgroundSize:"cover", height:'100vh',width:'100vw'  }} >
 				<img src={ logo } alt="main logo"/>
 				<Grid className="button-container" style={{ display:'flex', alignItems:'center', justifyContent:'center', margin:'auto' }}>
-					<Button variant="contained" style={{fontWeight:'bold'}} sx={{ p: 3, m: 2.8 }}>Create Room</Button>
-					<Button variant="contained" sx={{ p: 3, m: 2.8 }}>Join Room</Button>
+					<Button variant="contained" style={{fontWeight:'bold'}} sx={{ p: 3, m: 2.6 }}>Create Room</Button>
+					<Button variant="contained" style={{fontWeight:'bold'}} sx={{ p: 3, m: 2.6 }}>Join Room</Button>
 				</Grid>	
 			</div>	
 		</>
--- src/index.js
+++ src/index.js
@@ -4,11 +4,20 @@ import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux-toolkit/store';
 import { ThemeProvider, createTheme } from '@mui/material';
+import './style.css';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
 const theme = createTheme({
+	typography: {
+		fontFamily: [
+		  'Dosis',
+		  'BlinkMacSystemFont',
+		  '"Segoe UI"',
+		  'Roboto',
+		].join(','),
+	  },
 	palette: {
 		primary:{
 			light: "#665A9F",
