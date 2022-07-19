dd3362ce36f634c8ffea2bd91dc675d8edad6695
Stu <stuart.judd.bsc@gmail.com>
2022-07-19 17:16:29 +0100

    loading page with lolottie animation

--- src/Pages/LoadingPage/index.js
+++ src/Pages/LoadingPage/index.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import background from '../../assets/images/background8.jpg';
+import lottie from 'lottie-web';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography'
+
+
 
 const loadingPage = () => {
-    return <h1>About!</h1>
+    
+    const container = useRef(null);
+
+    useEffect(() => {
+        lottie.loadAnimation({
+            container: container.current,
+            render: 'svg',
+            loop: true,
+            autoplay: true,
+            animationData: require('../../assets/loading-icon.json')
+        })
+    }, [])
+
+    return (
+        <>
+        
+            <div className="background-container" style={{ backgroundImage: `url(${background})`, backgroundRepeat:"no-repeat",backgroundSize:"cover", height:'100vh',width:'100vw'  }}>
+
+           
+            
+            <Grid className="button-container" style={{ display:'flex', flexDirection: 'column', alignItems:'center', justifyContent:'center', margin:'auto', height: '100vh' }}>
+                 <Typography style={{fontSize: '4.8rem', fontWeight: 'bold', letterSpacing: '3px', color: 'white'}}>Loading Session</Typography>
+
+                <div style={{height: '450px'}} className='container' ref={container}>
+                </div>
+             </Grid>   
+            </div>
+        </>
+     )
 }
 
 export default loadingPage;
