e53ad190c33cc77807b0fc7909a2e97224b37084
Stu <stuart.judd.bsc@gmail.com>
2022-07-20 21:46:37 +0100

    race container 1.4

--- src/components/UserAvatar/index.js
+++ src/components/UserAvatar/index.js
@@ -10,46 +10,44 @@ const UserAvatar = () => {
 
     // FUNCTION TO DEFINE MOVEMENT
     const movementScore = () => {
-        
-    }
-    
 
-    const userContainer = useRef(null);
+    }
+    const userContainer1 = useRef(null);
 
     // PASS IN { userID } to animationData: require('../../assets/userAvatar{ userID }.json')
     useEffect(() => {
         lottie.loadAnimation({
-            container: userContainer.current,
+            container: userContainer1.current,
             render: 'svg',
             loop: true,
             autoplay: true,
-            animationData: require('../../assets/animations/userAvatar4.json')
+            animationData: require('../../assets/animations/userAvatar0.json')
         })
     }, [])
 
 
     return (
         <>
-         
-                    {/* MERGE THROUGH USERNAME HERE */}
-                     {players &&
-                            players.map((player, index) => {
-                            return (
-                                <div className='userRow' style={{height: '100px', display: 'flex', flexDirection: 'flex-start', }}>
-                                <div className='userName' style={{margin: 'auto 5px'}}>
-                                    <Typography style={{ color: 'white', fontSize:'1.2em', fontWeight: 'bold', margin:'auto'}}>
+           {players &&
+               players.map((player, index) => {
+                
+                return (
+                        <div key={index} className='userRow' style={{height: '100px', display: 'flex', flexDirection: 'flex-start', }}>
+                            <div className='cart' style={{display: 'flex', marginLeft: '30%'}}>   
+                                <div className='userName' style={{ margin: 'auto 5px'}}>
+                                    <Typography style={{ color: 'white', fontSize:'1.2em', fontWeight: 'bold', margin:'auto', textAlign: 'right' }}>
                                         {player.username} <br/>
                                         Score: { player.score } <br/>
                                         Index: { index }
                                     </Typography>
                                 </div>
-                                <div className='userContainer' ref={userContainer} style={{marginLeft: '0%'}}>
-                                {/* USER AVATAR GENERATES HERE */}
+                                <div ref={userContainer1} style={{ height: '115px'}}>
+                                          {/* ------------- USER AVATAR GENERATES HERE ---------   */}
                                 </div>
                             </div>
-                                
-                            );
-                            })}
+                      </div>
+                       );
+                    })}
 
         </>
     )
