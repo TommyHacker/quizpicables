f94cb9503fa3f6214954c8e7a652378ae19f2108
TommyHacker <t.hacker.webdev@gmail.com>
2022-07-21 16:10:03 +0100

    small changes

--- src/Pages/FinalResultPage/index.js
+++ src/Pages/FinalResultPage/index.js
@@ -1,24 +1,24 @@
-import React from "react";
-import ScoreBoard from "../../components/ScoreBoard";
-import background from "../../assets/images/background8.jpg";
+import React from 'react';
+import ScoreBoard from '../../components/ScoreBoard';
+import background from '../../assets/images/background8.jpg';
 
 const FinalResultPage = () => {
-  return (
-    <>
-      <div
-        className="background-container"
-        style={{
-          backgroundImage: `url(${background})`,
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-          height: "100vh",
-          width: "100vw",
-        }}
-      >
-         <ScoreBoard />
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div
+				className='background-container'
+				style={{
+					backgroundImage: `url(${background})`,
+					backgroundRepeat: 'no-repeat',
+					backgroundSize: 'cover',
+					height: '100vh',
+					width: '100vw',
+				}}
+			>
+				<ScoreBoard />
+			</div>
+		</>
+	);
 };
 
 export default FinalResultPage;
