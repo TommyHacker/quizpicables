c22e53f4655b59f646094698a3411ad6efbcffa6
TommyHacker <t.hacker.webdev@gmail.com>
2022-07-21 21:50:30 +0100

    added heroku url

--- src/helpers/socketClass.js
+++ src/helpers/socketClass.js
@@ -1,72 +1,72 @@
-const io = require("socket.io-client");
+const io = require('socket.io-client');
 const randomRoomId = () => {
-  return Math.floor(Math.random() * 399);
+	return Math.floor(Math.random() * 399);
 };
 
 class SocketControl {
-  constructor(username = "Host", isHost = false) {
-    this.url = "http://localhost:4041";
-    this.username = username;
-    this.roomNumber = 0;
-    this.isHost = isHost;
-    this.score = 0;
-    this.messages = [""];
-    this.gameData = {};
-    this.playersCount = 1;
-    this.playersInfo = [""];
-    global.socket = io.connect("http://localhost:4041");
-  }
-  connect() {}
-  updateRoomNumber(num) {
-    this.roomNumber = num;
-  }
-  updateUsername(name) {
-    this.username = name;
-  }
+	constructor(username = 'Host', isHost = false) {
+		this.url = 'https://quizpicable-server.herokuapp.com/';
+		this.username = username;
+		this.roomNumber = 0;
+		this.isHost = isHost;
+		this.score = 0;
+		this.messages = [''];
+		this.gameData = {};
+		this.playersCount = 1;
+		this.playersInfo = [''];
+		global.socket = io.connect('https://quizpicable-server.herokuapp.com/');
+	}
+	connect() {}
+	updateRoomNumber(num) {
+		this.roomNumber = num;
+	}
+	updateUsername(name) {
+		this.username = name;
+	}
 
-  join(id = this.roomNumber) {
-    socket.emit("join_room", {
-      roomNumber: id,
-      username: this.username,
-      isHost: this.isHost,
-      score: this.score,
-    });
-  }
+	join(id = this.roomNumber) {
+		socket.emit('join_room', {
+			roomNumber: id,
+			username: this.username,
+			isHost: this.isHost,
+			score: this.score,
+		});
+	}
 
-  sendMessage(messageText) {
-    try {
-      socket.emit("message", { messageText });
-    } catch (err) {
-      console.log(err);
-    }
-  }
+	sendMessage(messageText) {
+		try {
+			socket.emit('message', { messageText });
+		} catch (err) {
+			console.log(err);
+		}
+	}
 
-  giveAnswer(choice, correctAnswer) {
-    const data = { choice, correctAnswer, username: this.username };
-    socket.emit("giveAnswer", { data });
-  }
-  createRoom() {
-    const id = randomRoomId();
-    this.isHost = true;
-    this.roomNumber = id;
-    this.join(id);
-  }
-  disconnect() {
-    socket.emit("message", { data: `${this.username} has disconnected.` });
-    socket.disconnect();
-  }
-  host(val) {
-    switch (val) {
-      case true:
-        this.isHost = true;
-        break;
-      case false:
-        this.isHost = false;
-        break;
-      default:
-        return;
-    }
-  }
+	giveAnswer(choice, correctAnswer) {
+		const data = { choice, correctAnswer, username: this.username };
+		socket.emit('giveAnswer', { data });
+	}
+	createRoom() {
+		const id = randomRoomId();
+		this.isHost = true;
+		this.roomNumber = id;
+		this.join(id);
+	}
+	disconnect() {
+		socket.emit('message', { data: `${this.username} has disconnected.` });
+		socket.disconnect();
+	}
+	host(val) {
+		switch (val) {
+			case true:
+				this.isHost = true;
+				break;
+			case false:
+				this.isHost = false;
+				break;
+			default:
+				return;
+		}
+	}
 }
 
 // socket.on("message", ({ data }) => {
